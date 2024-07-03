fd191ba56c93ef0868fe600b2f14ecbb84462ff1
sVision <60247079+CoderAstrid@users.noreply.github.com>
2024-07-03 00:06:44 -0700

    init engine

---
+++ Yolo8Net/Engine/Label.cs
@@ -0,0 +1,16 @@
+﻿namespace Yolov8Net
+{
+    public class Label
+    {
+        public int Id { get; init; }
+        public string? Name { get; init; }
+        public LabelKind Kind { get; init; } = LabelKind.Generic;
+
+        //public Label(int id)
+        //{
+        //    Id = id;
+        //    Name = id.ToString();
+        //    Kind = LabelKind.Generic;
+        //}
+    }
+}
---
+++ Yolo8Net/Engine/Prediction.cs
@@ -0,0 +1,9 @@
+﻿namespace Yolov8Net
+{
+    public class Prediction
+    {
+        public Label? Label { get; init; }
+        public SixLabors.ImageSharp.RectangleF Rectangle { get; init; }
+        public float Score { get; init; }
+    }
+}
